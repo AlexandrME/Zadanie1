7cbcad737f3fb3a55e4afe27c3a7016d2ab3b9c6
Alexandr <manaev-2012@mail.ru>
2014-07-08 22:52:22 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +11,13 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             double a, b;
+            Console.WriteLine("Введите первое число");
             a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
             b = float.Parse(Console.ReadLine());
+            Console.WriteLine("среднее арифметическе =");
             Console.WriteLine("{0} ", 0.5 * (a + b));
+            Console.WriteLine("среднее геометрическое =");
             Console.WriteLine("{0} ", Math.Sqrt(a * b));
             Console.ReadKey(false);
         }
