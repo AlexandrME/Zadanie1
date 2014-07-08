85e229da2d95bb379af47f65cb5aefed8553ef7a
pavelsazhin <sergeivs11@mail.ru>
2014-07-08 23:04:00 +0600

    код программы
    добавил код программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("{0} ", 0.5 * (a + b));
+            Console.WriteLine("{0} ", Math.Sqrt(a * b));
+            Console.ReadKey(false);
+        }
+    }
+}
